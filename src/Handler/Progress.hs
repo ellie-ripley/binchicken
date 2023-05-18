21ceed6168056bdcdcf8266b22c410d9acff37c7
David Ripley <davewripley@gmail.com>
2023-05-18 08:59:16 +0200

    Mid working on scoring update; need to fix a bug

--- src/Handler/Progress.hs
+++ src/Handler/Progress.hs
@@ -21,47 +21,40 @@ import Import.NoFoundation
       ($),
       (.),
       fromIntegral,
-      Bounded(maxBound, minBound),
       Enum(fromEnum),
       Fractional((/)),
       Monad(return),
-      Num((+), (*)),
+      Num((*)),
       Ord((<=), (>=)),
       Show(show),
       Semigroup((<>)),
-      Bool(..),
       Double,
       Int,
       Maybe(Just, Nothing),
-      repeat,
       Text,
-      Map,
-      map,
-      foldr,
       null,
       pack,
       redirect,
       setTitle,
       Html,
-      zip,
-      Entity,
-      EntityField(AttemptSubmittedAt, AttemptUserId),
       EntityField(AttemptSubmittedAt, AttemptUserId),
       Yesod(defaultLayout),
       YesodPersist(runDB),
       widgetFile,
-      Attempt(attemptIsCorrect, attemptExerciseType),
       img_ibis_icon_png,
       YesodAuth(maybeAuthId) )
 import GHC.Float.RealFracMethods (roundDoubleInt)
-import Data.Map (adjust, fromList, lookup, foldlWithKey')
+import Data.Map (lookup)
 import qualified Database.Esqueleto.Legacy as E
 import Database.Esqueleto.Legacy ((^.), (==.))
 
 import Scoring
   ( PointsEarned(..)
   , Progress(..)
+  , displayPointsEarned
   , pointsEarned
+  , progressMap
+  , totalPoints
   )
 
 getProgressR :: Handler Html
@@ -82,8 +75,7 @@ getProgressR = do
             setTitle "Progress"
             $(widgetFile "progress")
 
-displayPointsEarned :: PointsEarned -> Text
-displayPointsEarned = pack . show . fromEnum
+
 
 exerciseProgressCurrentId :: ExerciseType -> Text
 exerciseProgressCurrentId et = (pack $ show et) <> "-progress-current"
@@ -97,28 +89,3 @@ streakBarPercent et bestStr
   | bestStr >= maxStr = 100
   | otherwise         = roundDoubleInt $ (fromIntegral $ bestStr * 100) / (fromIntegral maxStr :: Double)
   where maxStr = fullStreak et
-
-progressMap :: [Entity Attempt] -> Map ExerciseType Progress
-progressMap = (foldr accum zeroProgressMap) . (map E.entityVal)
-  where
-    zeroProgress :: Progress
-    zeroProgress = Progress 0 0 0
-
-    zeroProgressMap :: Map ExerciseType Progress
-    zeroProgressMap = fromList $ zip [minBound..maxBound] (repeat zeroProgress)
-
-    adj :: Bool -> Progress -> Progress
-    adj True (Progress cur bes tot) =
-      Progress (cur + 1) (if bes <= cur
-                          then cur + 1
-                          else bes) (tot + 1)
-    adj False (Progress _ bes tot) = Progress 0 bes tot
-
-    accum :: Attempt -> Map ExerciseType Progress -> Map ExerciseType Progress
-    accum a m = adjust (adj $ attemptIsCorrect a) (attemptExerciseType a) m
-
-totalPoints :: Map ExerciseType Progress -> Int
-totalPoints = foldlWithKey' accum 0
-  where
-    accum :: Int -> ExerciseType -> Progress -> Int
-    accum a et p = a + (fromEnum $ pointsEarned (targets et) p)
--- src/Scoring.hs
+++ src/Scoring.hs
@@ -3,22 +3,41 @@
 module Scoring where
 
 import Import.NoFoundation
-  ( (||)
+  ( (+)
+  , (.)
+  , fmap
+  , foldl'
+  , foldr
+  , fromMaybe
+  , map
+  , max
   , otherwise
-  , Bool
-  , Enum
+  , repeat
+  , zip
+  , Attempt(..)
+  , Bool(..)
+  , Entity(..)
   , Eq
   , Int
-  , Ord((>=))
+  , Key(..)
+  , Maybe(..)
+  , Ord((>=), (<=))
   , Read
-  , Show
+  , Show(..)
+  , Text
+  , User(..)
   )
+
+import Data.Map (Map)
+import qualified Data.Map                  as M
 
 import ExerciseType
   ( ExerciseTargets(..)
+  , ExerciseType(..)
   )
+import Settings.Binchicken (activeExerciseTypes, targets)
 
-
+-- | Information about a single user's progress on a single exercise type
 data Progress =
   Progress { currentStreak :: Int
            , bestStreak    :: Int
@@ -26,33 +45,112 @@ data Progress =
            }
   deriving (Eq, Read, Show)
 
-data TargetsReached =
-  TargetsReached { target1 :: Bool
-                 , target2 :: Bool
-                 , target3 :: Bool
-                 , target4 :: Bool
-                 }
+-- | Information about a single user's overall progress
+type Results = Map ExerciseType Progress
 
-targetsReached :: ExerciseTargets -> Progress -> TargetsReached
-targetsReached (ExerciseTargets t1 t2 s1) (Progress _ bes tot) =
-  TargetsReached (tot >= 1  || bes >= s1)
-                 (tot >= t1 || bes >= s1)
-                 (tot >= t2 || bes >= s1)
-                 (bes >= s1)
+type ResultsCalculated = Map ExerciseType Int
 
-data PointsEarned =
-    ZeroPoints
-  | OnePoint
-  | TwoPoints
-  | ThreePoints
-  | FourPoints
-  deriving (Eq, Ord, Enum)
+data SummaryRow = SummaryRow
+  { srEmail :: Text
+  , srResults :: Results
+  }
 
-pointsEarned :: ExerciseTargets -> Progress -> PointsEarned
-pointsEarned et pr
-  | tr4 = FourPoints
-  | tr3 = ThreePoints
-  | tr2 = TwoPoints
-  | tr1 = OnePoint
-  | otherwise = ZeroPoints
-  where (TargetsReached tr1 tr2 tr3 tr4) = targetsReached et pr
+data SummaryRowCalculated = SummaryRowCalculated
+  { srcEmail :: Text
+  , srcResultsCalculated :: ResultsCalculated
+  }
+
+-- | Information about a collection of users' progress
+type Summary = Map (Key User) SummaryRow
+
+type SummaryCalculated = Map (Key User) SummaryRowCalculated
+
+-- | Results at the start of time
+emptyResults :: Results
+emptyResults =
+  let startRT = Progress 0 0 0
+  in  foldl' (\acc et -> M.insert et startRT acc) M.empty activeExerciseTypes
+
+-- | Given a user, an empty row for that user
+emptyRow :: User -> SummaryRow
+emptyRow u = SummaryRow
+  { srEmail = userEmail u
+  , srResults = emptyResults
+  }
+
+-- | Given a list of users, an empty summary for those users
+emptySumm :: [Entity User] -> Summary
+emptySumm [] = M.empty
+emptySumm ((Entity i u):eus) =
+  let runningS = emptySumm eus
+  in case M.lookup i runningS of
+    Nothing -> M.insert i (emptyRow u) runningS
+    Just _  -> runningS
+
+-- | what one correct answer does to a Progress
+correctUpdate :: Progress -> Progress
+correctUpdate prog =
+  let newCorr = totalCorrect prog + 1
+      newCurrStreak = currentStreak prog + 1
+      newBestStreak = max newCurrStreak (bestStreak prog)
+  in Progress { totalCorrect = newCorr
+              , bestStreak = newBestStreak
+              , currentStreak = newCurrStreak
+              }
+
+-- | what one incorrect answer does to a Progress
+incorrectUpdate :: Progress -> Progress
+incorrectUpdate prog = prog { currentStreak = 0 }
+
+
+-- | Adds an attempt to a row, given a Bool saying if the attempt was correct
+-- ignores exercise types not already present in the row (in practice, inactives)
+addAttempt
+  :: ExerciseType
+  -> Bool
+  -> SummaryRow
+  -> SummaryRow
+addAttempt et corr sr = sr { srResults = newResults }
+  where
+    newResults = let upd = if corr then correctUpdate else incorrectUpdate
+                 in M.adjust upd et (srResults sr)
+
+-- | Takes full db of attempts and of users to create summary table
+-- | only counts attempts where the user is present in the list of users
+tally :: [Entity User] -> [Entity Attempt] -> Summary
+tally usrs = foldl' inserter (emptySumm usrs)
+  where
+    inserter :: Summary -> Entity Attempt -> Summary
+    inserter srs (Entity _ att) =
+      case attemptUserId att of -- does the attempt have a user id?
+        Nothing  -> srs
+        Just uid -> M.adjust (addAttempt (attemptExerciseType att) (attemptIsCorrect att)) uid srs
+
+-- | Functions for taking Progress and moving to actual scores
+
+pointsEarned :: ExerciseTargets -> Progress -> Int
+pointsEarned (ExerciseTargets tot1 tot2 str) (Progress _ bes tot)
+  | bes >= str  = 4
+  | tot >= tot2 = 3
+  | tot >= tot1 = 2
+  | tot >= 1    = 1
+  | otherwise   = 0
+
+calculateResults :: Results -> ResultsCalculated
+calculateResults = M.mapWithKey (pointsEarned . targets)
+
+calculateSRow :: SummaryRow -> SummaryRowCalculated
+calculateSRow (SummaryRow em res) = SummaryRowCalculated em (calculateResults res)
+
+calculateSummary :: Summary -> SummaryCalculated
+calculateSummary = map calculateSRow
+
+
+exScore :: SummaryRowCalculated -> ExerciseType -> Maybe Int
+exScore src et = M.lookup et (srcResultsCalculated src)
+
+totalPoints :: SummaryRowCalculated -> Maybe Int
+totalPoints src = foldr adder (Just 0) (srcResultsCalculated src)
+  where
+    adder :: Int -> Maybe Int -> Maybe Int
+    adder i = fmap (i +)
